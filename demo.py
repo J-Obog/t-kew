29c693372f3c16a512f9341d73f3a99e02d409d8
JObog <jobogbaimhe@gmail.com>
2021-12-08 05:29:18 -0500

    created delegator thread to delegate incoming tasks in queue

--- demo.py
+++ demo.py
@@ -4,20 +4,25 @@ import random
 
 def expensive_task(timeout):
     time.sleep(timeout)
+    print(f'Finished with {timeout}')
 
 
 def main():
     s = time.perf_counter()
-    tq = TaskQueue(1)
+    tq = TaskQueue(5)
     tq.start() 
 
     num_taks = 10
     min_timeout = 1
     max_timeout = 15
+    sm = 0
 
     for _ in range(num_taks): 
         timeout = random.randint(min_timeout, max_timeout)
+        sm += timeout
         tq.queue(expensive_task, timeout)
+
+    print(sm)
 
     tq.stop()
     e = time.perf_counter()
--- tkew.py
+++ tkew.py
@@ -7,6 +7,8 @@ DEFAULT_WORKER_SIZE = cpu_count() // 2
 class TaskQueue:
     def __init__(self, num_workers: int = DEFAULT_WORKER_SIZE):
         self.__workers: List[Thread] = [Thread(target=self.__listen, args=(wid,) ) for wid in range(num_workers)]
+        self.__delegator: Thread = Thread(target=self.__delegate)
+        self.__q: List[Tuple] = []
         self.__jobs: List[Optional[Tuple]] = [None] * num_workers
         self.__signal: bool = False
     
@@ -26,18 +28,26 @@ class TaskQueue:
             i += 1
         return i % n
 
+    def __delegate(self, *args, **kwargs):
+        while not self.__signal:
+            if len(self.__q) != 0:
+                wid = self.__poll()
+                self.__jobs[wid] = self.__q[0]
+                self.__q.pop(0)
+
     def start(self):
+        self.__delegator.start()
         for worker in self.__workers:
             worker.start()
 
     def stop(self):
         while any(self.__jobs):
             pass
-
+        
         self.__signal = True
+        self.__delegator.join()
         for worker in self.__workers:
             worker.join()
 
     def queue(self, fn, *args, **kwargs):
-        wid = self.__poll()
-        self.__jobs[wid] = (fn, args, kwargs)
+        self.__q.append((fn, args, kwargs))
