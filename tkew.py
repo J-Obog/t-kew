f08d7ed4fd9c3c36f595a0a9f66f70ad60747d0f
JObog <jobogbaimhe@gmail.com>
2021-12-07 21:41:02 -0500

    getting down basic structure of the constructor

--- tkew.py
+++ tkew.py
@@ -1,3 +1,11 @@
+from threading import Thread
+from multiprocessing import cpu_count
+from typing import List, Tuple, Optional
+
+DEFAULT_WORKER_SIZE = cpu_count() // 2 
 
 class TaskQueue:
-    pass
+    def __init__(self, num_workers: int = DEFAULT_WORKER_SIZE):
+        self.__workers: List[Thread] = [Thread(target=None, args=(wid,) ) for wid in range(num_workers)]
+        self.__flags: List[Optional[Tuple]] = [None] * num_workers
+        self.__signal: bool = True
