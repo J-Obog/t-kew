a2759b8e9f9154959b115c7ed09a2633f2b2e615
JObog <jobogbaimhe@gmail.com>
2021-12-07 22:08:05 -0500

    adding timer to output program's running time

--- demo.py
+++ demo.py
@@ -1,16 +1,19 @@
 from tkew import TaskQueue
-from time import sleep
+import time
 
 def expensive_task(timeout):
-    sleep(timeout)
+    time.sleep(timeout)
 
 def main():
+    s = time.perf_counter()
     tq = TaskQueue(1)
     tq.start() 
 
     tq.queue(expensive_task, 3)
 
     tq.stop()
+    e = time.perf_counter()
+    print(f'Program ran in {e - s} seconds')
 
 
 if __name__ == '__main__':
