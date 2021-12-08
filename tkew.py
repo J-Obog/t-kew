93991553125cb18e6b6fb6c38e22d7a3f6de2a55
JObog <jobogbaimhe@gmail.com>
2021-12-07 21:57:56 -0500

    listen method checks if a service has work todo

--- tkew.py
+++ tkew.py
@@ -6,9 +6,18 @@ DEFAULT_WORKER_SIZE = cpu_count() // 2
 
 class TaskQueue:
     def __init__(self, num_workers: int = DEFAULT_WORKER_SIZE):
-        self.__workers: List[Thread] = [Thread(target=None, args=(wid,) ) for wid in range(num_workers)]
+        self.__workers: List[Thread] = [Thread(target=self.__listen, args=(wid,) ) for wid in range(num_workers)]
         self.__jobs: List[Optional[Tuple]] = [None] * num_workers
         self.__signal: bool = False
+    
+    def __listen(self, *args, **kwargs):
+        wid = args[0]
+        while not self.__signal:
+            job = self.__jobs[wid] 
+            if job != None:
+                fn, ags, kwa = job
+                fn(*ags, **kwa) 
+                self.__jobs[wid] = None
 
     def __poll(self) -> int:
         i = 0
